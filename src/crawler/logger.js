0874f2e47d9674894f02918cce1f18fe4d2438c7
Nico <nico.kruger@gmail.com>
2023-06-29 09:07:42 +0200

    try to get winston to do the sane thing

--- src/crawler/logger.js
+++ src/crawler/logger.js
@@ -1,12 +1,48 @@
 // logger.js
 const winston = require('winston');
 const path = require('path');
+const util = require('util');
 const DailyRotateFile = require('winston-daily-rotate-file');
 
+//https://github.com/winstonjs/winston/issues/1427
+const enumerateErrorFormat = winston.format(info => {
+    if (info.message instanceof Error) {
+      info.message = Object.assign({
+        message: info.message.message,
+        stack: info.message.stack
+      }, info.message);
+    }
+
+    if (info instanceof Error) {
+      return Object.assign({
+        message: info.message,
+        stack: info.stack
+      }, info);
+    }
+
+    return info;
+});
+
+const plsFormat = {
+            transform: (info) => {
+                const args = [info.message, ...(info[Symbol.for('splat')] || [])];
+                info.message = args;
+
+                const msg = args.map(arg => {
+                    if (typeof arg == 'object')
+                        return util.inspect(arg, {compact: true, depth: Infinity});
+                    return arg;
+                }).join(' ');
+
+                info[Symbol.for('message')] = `${info[Symbol.for('level')]}: ${msg}${info.stack ? ' ' + info.stack : ''}`;
+
+                return info;
+            }
+        }
 const createLogger = (module) => {
     const filename = path.basename(module.filename);
 
-    const logFormat = winston.format.printf(({ level, message, timestamp, label }) => {
+    const logFormat = winston.format.printf(({ timestamp, label, level, message, splat, ...rest }) => {
         return `[${timestamp}] ${label} [${level}] ${message}`;
     });
 
@@ -15,6 +51,7 @@ const createLogger = (module) => {
         format: winston.format.combine(
             winston.format.label({ label: filename }),
             winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+            plsFormat,
             logFormat
         ),
         transports: [
