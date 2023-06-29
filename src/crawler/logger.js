28626e70e1a292311bc56930019a6c863d9b0cbf
Nico <nico.kruger@gmail.com>
2023-06-29 07:14:11 +0200

    added file logger

--- src/crawler/logger.js
+++ src/crawler/logger.js
@@ -1,6 +1,7 @@
 // logger.js
 const winston = require('winston');
 const path = require('path');
+const DailyRotateFile = require('winston-daily-rotate-file');
 
 const createLogger = (module) => {
     const filename = path.basename(module.filename);
@@ -17,7 +18,14 @@ const createLogger = (module) => {
             logFormat
         ),
         transports: [
-            new winston.transports.Console()
+            new winston.transports.Console(),
+            new DailyRotateFile({
+                filename: 'logs/%DATE%.log',
+                datePattern: 'YYYY-MM-DD',
+                zippedArchive: true,
+                maxSize: '20m',
+                maxFiles: '7d' // keep logs for 7 days
+            })
         ],
     });
 
